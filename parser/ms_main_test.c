16a33d051f9cdd9462eac82ecc26d66ab3a6d6ac
gsaiago <gsaiago@student.42.rio>
2022-12-01 21:43:18 -0300

    honestamente? Tá sendo um inferno fazer o parser

--- parser/ms_main_test.c
+++ parser/ms_main_test.c
@@ -6,26 +6,35 @@
 /*   By: gsaiago <gsaiago@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 20:26:37 by gsaiago           #+#    #+#             */
-/*   Updated: 2022/11/28 21:28:07 by gsaiago          ###   ########.fr       */
+/*   Updated: 2022/12/01 17:56:20 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int	count_words(char *line);
+int	count_word_len(char *word);
+char	**ms_split_input(char *line);
+
 int	main(void)
 {
 	char	*str;
-	char	**phrase_mat;
+	char	**mat;
 	int		i;
 
 	i = 0;
 	str = readline("Insert input$ ");
-	phrase_mat = ms_split_phrases(str, 1);
-	while (phrase_mat[i])
+	mat = ms_split_input(str);
+	printf("count words > |%d|\n", count_words(str));
+	printf("word len > |%d|\n", count_word_len(str));
+	while (mat[i])
 	{
-		printf("Phrase %d > |%s|\n", i, phrase_mat[i]);
+		printf("mat[%d] > %s\n", i, mat[i]);
 		i++;
 	}
-	ft_free_mat(phrase_mat);
+
+
+	free(str);
+//	ft_free_mat(mat);
 	return (0);
 }
--- parser/ms_validate_input.c
+++ parser/ms_validate_input.c
@@ -6,53 +6,192 @@
 /*   By: gsaiago <gsaiago@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 11:36:25 by gsaiago           #+#    #+#             */
-/*   Updated: 2022/11/28 21:39:29 by gsaiago          ###   ########.fr       */
+/*   Updated: 2022/12/01 21:42:14 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-/*
-char	***create_argv_array(t_shell s_shell)
+int	count_words(char *line);
+int	count_word_len(char *word);
+int	next_quotes(char *line);
+int	is_meta_character(char c);
+int	count_meta_len(char *line);
+char	**ms_split_input(char *line);
+
+int	count_words(char *line)
 {
-	char	***argv_array;
-	int		phrases;
+	int		words;
 	int		i;
 
 	i = 0;
-	if (s_shell->input_line)
-		return (NULL);
-	phrases = count_phrases(s_shell->input_line);
-	argv_array = ft_calloc(sizeof(char *), phrases + 1);
-	if (!argv_array)
-		return (NULL);
-	while (i < phrases)
+	words = 0;
+	while (line[i])
 	{
-		argv_array[i] = 
+		while (line[i] && ft_isspace(line[i]))
+			i++;
+		while (line[i] && !ft_isspace(line[i]))
+		{
+			if (line[i] == '\'' || line[i] == '\"')
+				i += next_quotes(&line[i]) + 1;
+			if (is_meta_character(line[i]))
+			{
+				if (i > 0 && !ft_isspace(line[i-1]))
+					words++;
+				i += count_meta_len(&line[i]);
+				break ;
+			}
+			i++;
+		}
+		words++;
+	}
+	return (words);
+}
+
+int	count_word_len(char *word)
+{
+	int	len;
+
+	if (!word || !word[0])
+		return (0);
+	len = 0;
+	while (ft_isspace(*word))
+		word++;
+	if (is_meta_character(*word))
+		return (count_meta_len(word));
+	while (word[len])
+	{
+		if (word[len] == '\"' || word[len] == '\'')
+			len += next_quotes(word + len) + 1;
+		else if (!ft_isspace(word[len]) && !is_meta_character(word[len]))
+			len++;
+		else
+			break ;
+	}
+	return (len);
+}
+
+int	count_meta_len(char *line)
+{
+	int meta;
+
+	if (!line || !is_meta_character(*line))
+		return (0);
+	meta = is_meta_character(*line);
+	if (ft_strlen(line) >= 2)
+	{
+		if (meta == line[1]) 
+			return (2);
+	}
+	return (1);
+}
+
+int	next_quotes(char *line)
+{
+	if (!line)
+		return (0);
+	if (*line == '\"')
+		return (ft_strchrn(line + 1, '\"'));
+	else if (*line == '\'')
+		return (ft_strchrn(line + 1, '\''));
+	else
+		return (0);
+}
+
+int	is_meta_character(char c)
+{
+	if (c == '&')
+		return ('&');
+	else if (c == '|')
+		return ('|');
+	else if (c == '>')
+		return ('>');
+	else if (c == '<')
+		return ('<');
+	else
+		return (0);
+}
+
+char	**ms_split_input(char *line)
+{
+	int		i;
+	int		word_len;
+	int		word_count;
+	char	**mat;
+
+	i = 0;
+	word_len = 0;
+	if (!line)
+		return (NULL);
+	word_count = count_words(line);
+	mat = ft_calloc(sizeof(char *), (word_count + 1));
+	if (!mat)
+		return (NULL);
+	while (i < word_count)
+	{
+		while (ft_isspace(*line))
+			line++;
+		word_len = count_word_len(line);
+		mat[i] = ft_substr(line, 0, word_len);
+		if (!mat[i])
+		{
+			ft_free_mat(mat);
+			return (NULL);
+		}
+		line += word_len;
 		i++;
 	}
-
+	return (mat);
 }
 
-char **create_argv(t_shell s_shell)
+/*
+int	env_len(char *line, int start, int end)
 {
-	char	**av;
-	int		i;
+	int 	env_len;
+	char	*env_var;
+	char	*env;
 
-	if (!s_shell->input_line)
-		return (NULL);
+	while (*line && && (*l)
 
-}
 
-char	**minishell_split(t_shell s_shell)
-{
-	int		quotes_len;
-	int		i;
-	char	**av;
-
-	
 }
 */
+
+/*
+char	**ms_split_phrases(char *line, int flag)
+{
+	int		i;
+	int		line_index;
+	int		len;
+	int		n_phrases;
+	char	**phrase_array;
+
+	if (!line)
+		return (NULL);
+	i = -1;
+	line_index = 0;
+	n_phrases = count_phrases(line);
+	phrase_array = (char **)malloc((n_phrases + 1) * sizeof(char *));
+	if (!phrase_array)
+		return (NULL);
+	while (++i < n_phrases)
+	{
+		while (ft_isspace(line[line_index]))
+			line_index++;
+		len = count_phrase_len(line + line_index);
+		phrase_array[i] = ft_substr(line, line_index, len);
+		if (!phrase_array[i])
+		{
+			ft_free_mat(phrase_array);
+			return (NULL);
+		}
+		line_index += len;
+	}
+	phrase_array[i] = NULL;
+	if (flag)
+		free (line);
+	return (phrase_array);
+}
 
 int	count_phrases(char *line)
 {
@@ -105,117 +244,4 @@ int	count_phrase_len(char *line)
 	}
 	return (i);
 }
-
-int	count_word_len(char *phrase)
-{
-	int	i;
-	int	len;
-	int	env_len;
-
-	if (!phrase || !phrase[0])
-		return (0);
-	i = 0;
-	len = 0;
-	while (ft_isspace(phrase[i]))
-		i++;
-	while (phrase[i + len])
-	{
-		if (phrase[i + len] == '\"' || phrase[i + len] == '\'')
-			len = next_quotes(phrase + i + 1);
-		else if (!ft_isspace(phrase[i + len]))
-			len++;
-		else
-			break ;
-	}
-	len += env_len;
-	return (len);
-}
-/*
-int	env_len(char *line, int start, int end)
-{
-	int 	env_len;
-	char	*env_var;
-	char	*env;
-
-	while (*line && && (*l)
-
-
-}
 */
-
-int	next_quotes(char *line)
-{
-	if (!line)
-		return (0);
-	if (*line == '\'')
-		return (ft_strchrn(line + 1, '\"'));
-	else if (*line == '\'')
-		return (ft_strchrn(line + 1, '\''));
-	else
-		return (0);
-}
-
-int	count_words(char *line)
-{
-	int		words;
-
-	while (ft_isspace(*line))
-		line++;
-	while (*line)
-	{
-		if (*line == '\'' || *line == '\"')
-			line += next_quotes(line);
-		line++;
-		words++;
-	}
-	return (words);
-}
-
-int	is_meta_character(char c)
-{
-	if (c == '&')
-		return ('&');
-	else if (c == '|')
-		return ('|');
-	else if (c == '>')
-		return ('>');
-	else if (c == '<')
-		return ('<');
-	else
-		return (0);
-}
-
-char	**ms_split_phrases(char *line, int flag)
-{
-	int		i;
-	int		line_index;
-	int		len;
-	int		n_phrases;
-	char	**phrase_array;
-
-	if (!line)
-		return (NULL);
-	i = -1;
-	line_index = 0;
-	n_phrases = count_phrases(line);
-	phrase_array = (char **)malloc((n_phrases + 1) * sizeof(char *));
-	if (!phrase_array)
-		return (NULL);
-	while (++i < n_phrases)
-	{
-		while (ft_isspace(line[line_index]))
-			line_index++;
-		len = count_phrase_len(line + line_index);
-		phrase_array[i] = ft_substr(line, line_index, len);
-		if (!phrase_array[i])
-		{
-			ft_free_mat(phrase_array);
-			return (NULL);
-		}
-		line_index += len;
-	}
-	phrase_array[i] = NULL;
-	if (flag)
-		free (line);
-	return (phrase_array);
-}
