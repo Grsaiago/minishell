25f6a2686866da2eb0de66fceb5bef653095ce01
gsaiago <gsaiago@student.42.rio>
2023-02-24 13:41:57 -0300

    Parser reinitialized

---
+++ parser/main_parser.c
@@ -0,0 +1,8 @@
+#include "minishell.h"
+
+int	main(void)
+{
+	char	*rdline;
+
+	rdline = readline("$> ");
+}
---
+++ parser/minishell.h
@@ -0,0 +1,5 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "../libft/libft.h"
+
+# endif
