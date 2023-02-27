5a10e492a812a46653e119c289574cc935b8ca04
gsaiago <gsaiago@student.42.rio>
2023-02-26 23:34:51 -0300

    splitting complete

--- parser/main_parser.c
+++ parser/main_parser.c
@@ -1,14 +1,18 @@
 #include "minishell.h"
-int	ms_ismeta(char *c);
-int	ms_count_words(char *line);
+int		ms_ismeta(char *c);
+int		ms_count_words(char *line);
+char	**ms_split(char *line);
+int		get_word_len(char *line);
 
 int	main(void)
 {
 	char	*line;
+	char	**mat;
 
 	line = readline("$> ");
-	printf("Essa foi a linha: %s\nE as palavras são: |%d|\n", line, ms_count_words(line));
+	mat = ms_split(line);
 	free(line);
+	ft_free_mat(mat);
 }
 
 int	ms_count_words(char *line)
@@ -48,12 +52,43 @@ int	ms_ismeta(char *c)
 char	**ms_split(char *line)
 {
 	int		words;
+	int		word_len;
+	int		i;
 	char	**mat;
 
 	words = ms_count_words(line);
 	if (words)
 		mat = ft_calloc(sizeof(char *), (words + 1));
+	printf("Words on | %s | > %d\n", line, words); //debug
 	if (!mat)
 		return (NULL);
+	i = 0;
+	while (ft_isspace(*line))
+		line++;
+	while (*line)
+	{
+		word_len = get_word_len(line);
+		mat[i] = ft_substr(line, 0, word_len); 
+		line += word_len;
+		while (ft_isspace(*line))
+			line++;
+		printf("mat[%d] > %s\n", i, mat[i]); //debug
+		i++;
+	}
 	return (mat);
+}
+
+int	get_word_len(char *line)
+{
+	int	word_len;
+
+	if (ms_ismeta(line))
+		return (ms_ismeta(line));
+	word_len = 0;
+	while (*line && !ms_ismeta(line) && !ft_isspace(*line))
+	{
+		line++;
+		word_len++;
+	}
+	return (word_len);
 }
