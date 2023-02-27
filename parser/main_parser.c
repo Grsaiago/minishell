dc7a3c1d817f3ef5dcb3e3b66f63fcb1cc224cde
gsaiago <gsaiago@student.42.rio>
2023-02-26 21:39:49 -0300

    Word count on parser working

--- parser/main_parser.c
+++ parser/main_parser.c
@@ -1,8 +1,46 @@
 #include "minishell.h"
+int	ms_ismeta(char *c);
+int	ms_count_words(char *line);
 
 int	main(void)
 {
-	char	*rdline;
+	char	*line;
 
-	rdline = readline("$> ");
+	line = readline("$> ");
+	printf("Essa foi a linha: %s\nE as palavras são: |%d|\n", line, ms_count_words(line));
+	free(line);
+}
+
+int	ms_count_words(char *line)
+{
+	unsigned int	words;
+
+	while (*line && ft_isspace(*line))
+		line++;
+	words = 0;
+	while (*line)
+	{
+		words++;
+		while (ft_isspace(*line))
+			line++;
+		if (ms_ismeta(line))
+			line += ms_ismeta(line);
+		else if (*line && !ft_isspace(*line))
+		{
+			while (*line && !ft_isspace(*line) && !ms_ismeta(line))
+				line++;
+		}
+		while (ft_isspace(*line))
+			line++;
+	}
+	return (words);
+}
+
+int	ms_ismeta(char *c)
+{
+	if ((c[0] == '<' && c[1] == '<') || (c[0] == '>' && c[1] == '>'))
+		return (2);
+	else if (*c == '|' || *c == '>' || *c == '<')
+		return (1);
+	return (0);
 }
--- parser/minishell.h
+++ parser/minishell.h
@@ -1,5 +1,13 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <locale.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 # endif
