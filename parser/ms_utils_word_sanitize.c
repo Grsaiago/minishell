b7da7f909cbea5dbe334599181227f6fc1b1fd5b
gsaiago <gsaiago@student.42.rio>
2023-03-13 18:53:30 -0300

    fix(parser): Add the get_env_word_len for count

--- parser/ms_utils_word_sanitize.c
+++ parser/ms_utils_word_sanitize.c
@@ -1,4 +1,5 @@
 #include "minishell.h"
+int	get_dollar_word_len(char *line);
 
 /*
 char	*ms_expand_env(char *line);
@@ -22,15 +23,15 @@ int	ms_get_len_after_expansion(char *line)
 	{
 		if (*line == '\'')
 		{
-			final_len += ms_find_next_quotes(line) + 1;
+			final_len += ms_find_next_quotes(line) + 2;
 			line += ms_find_next_quotes(line) + 1;
 		}
 		else if (*line == '$')
 		{
-			env_word = ft_substr(line, 1, get_word_len(line) - 1);
+			env_word = ft_substr(line, 1, get_dollar_word_len(line));
 			final_len += ft_strlen(getenv(env_word));
 			printf("Env_word > |%s|\nFinal_len > %d\n", env_word, final_len); //debug
-			line += get_word_len(line);
+				line += get_dollar_word_len(line) + 1;
 			if (env_word)
 				free(env_word);
 		}
@@ -42,4 +43,20 @@ int	ms_get_len_after_expansion(char *line)
 	}
 	printf("final_len > |%d|\n", final_len); //debug
 	return (final_len);
+}
+
+int	get_dollar_word_len(char *line)
+{
+	int	word_len;
+
+	if (!line)
+		return (0);
+	word_len = 0;
+	line++;
+	while (line && *line && ft_isalpha(*line))
+	{
+		word_len++;
+		line++;
+	}
+	return (word_len);
 }
