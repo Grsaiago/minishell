8032e6dda24e53ec43036538a0c9963eecbd4fa1
gsaiago <gsaiago@student.42.rio>
2023-03-18 22:42:06 -0300

    Normm(parser): subdivided the functions in different files

--- parser/ms_parser.c
+++ parser/ms_parser.c
@@ -6,7 +6,7 @@
 /*   By: gsaiago <gsaiago@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 11:42:31 by gsaiago           #+#    #+#             */
-/*   Updated: 2023/03/07 16:35:52 by gsaiago          ###   ########.fr       */
+/*   Updated: 2023/03/18 22:32:36 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,17 @@ int	ms_validate_quote_ammount(char *line);
 int	ms_parser(char *line, t_word **word_lst)
 {
 	*word_lst = NULL;
-	if (line)
+	if (ms_validate_quote_ammount(line))
 	{
-		if (ms_validate_quote_ammount(line))
-		{
-			free(line);
-			printf("Error!\nThere is an unclosed quote\n");
-		}
-		else
-		{
-			*word_lst = ms_create_word_lst(line);
-			free(line);
-			if (!word_lst)
-				return (1);
-			//ms_word_lst_flag_init(*word_lst);
-			//ms_lst_remove_if(word_lst);
-		}
+		printf("Syntax error: There is an unclosed quote\n");
+		free(line);
+		return (1);
 	}
+	*word_lst = ms_create_word_lst(line);
+	if (!word_lst)
+		return (1);
+	//ms_word_lst_flag_init(*word_lst);
+	//ms_lst_remove_if(word_lst);
+	free(line);
 	return (0);
 }
--- parser/ms_utils_expand_env.c
+++ parser/ms_utils_expand_env.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
-int	ms_get_env_name_len(char *line);
-int	ms_get_len_after_expansion(char *line);
+int		ms_get_env_name_len(char *line);
+int		ms_get_len_after_expansion(char *line);
 char	*ms_expand_env(char *line);
 void	ms_expand_env_util(char *line, int i, int j, char *ret_line);
 int		ms_validate_env_name(char *line);
@@ -95,55 +95,4 @@ int	ms_get_env_name_len(char *line)
 		line++;
 	}
 	return (word_len);
-}
-
-/* daqui pra baixo tudo certin ok ?????? NÃO MEXE CARAIO*/
-char	*ms_remove_quotes(char *line, int flag)
-{
-	char	*ret_word;
-	int		i;
-	int		len;
-
-	if (!line)
-		return (NULL);
-	len = ft_strlen(line);
-	ret_word = NULL;
-	i = 0;
-	if (!(ft_strchr(line, '\'') || ft_strchr(line, '\"')))
-		return (line);
-	ms_null_start_end_quotes(line);
-	while (!line[i] && i < len)
-		i++;
-	while(i < len)
-	{
-		ret_word = ft_strjoin(ret_word, &line[i], 1);
-		i += ft_strlen(&line[i]) + 1;
-		while (!line[i] && i < len)
-			i++;
-	}
-	if (flag)
-		free(line);
-	return (ret_word);
-}
-
-void	ms_null_start_end_quotes(char *line)
-{
-	int	next_quotes;
-
-	if (!line)
-		return ;
-	while (*line)
-	{
-		if (*line == '\'' || *line == '\"')
-		{
-			next_quotes = ms_find_next_quotes(line);
-			*line = '\0';
-			line += next_quotes;
-			*line = '\0';
-			line++;
-		}
-		else
-			line++;
-	}
-	return ;
 }
