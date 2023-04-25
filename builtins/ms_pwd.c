ffd8852aadfb048b7db68a40cbea871eb189e087
Gabriel Saiago <grsaiago@gmail.com>
2023-04-25 18:04:49 -0300

    General fix on builtins (#73)
    * Add(builtin): add cd wih no args and fixed sigseg
    
    * Fix(pwd): no newline after pwd
    
    ---------
    
    Co-authored-by: gsaiago <gsaiago@student.42.rio>

--- builtins/ms_pwd.c
+++ builtins/ms_pwd.c
@@ -6,7 +6,7 @@
 /*   By: gsaiago <gsaiago@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 11:54:02 by gsaiago           #+#    #+#             */
-/*   Updated: 2023/04/23 16:22:35 by gsaiago          ###   ########.fr       */
+/*   Updated: 2023/04/25 17:13:10 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	ms_pwd(t_word *node)
 	if (!cwd)
 		return (1);
 	ft_putstr_fd(cwd, node->fd_out);
+	ft_putchar_fd('\n', node->fd_out);
 	free(cwd);
 	return (0);
 }
