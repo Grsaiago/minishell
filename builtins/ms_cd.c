f51e20f73b3893e14d743b657896924e63621d7a
gsaiago <gsaiago@student.42.rio>
2023-04-25 17:10:58 -0300

    Add(builtin): add cd wih no args and fixed sigseg

--- builtins/ms_cd.c
+++ builtins/ms_cd.c
@@ -6,21 +6,55 @@
 /*   By: gsaiago <gsaiago@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 23:27:57 by gsaiago           #+#    #+#             */
-/*   Updated: 2023/04/23 16:22:03 by gsaiago          ###   ########.fr       */
+/*   Updated: 2023/04/25 17:07:52 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+static int	ms_cd_without_params(t_word *node);
+static int	ms_cd_with_params(t_word *node);
 
 int	ms_cd(t_word *node)
 {
-	char	*pwd;
+	uint8_t	err;
 
-	if (node->next && node->next->next->flag != MS_WORD)
+	if (node->next && node->next->next && node->next->next->flag != MS_WORD)
 	{
 		ft_putstr_fd("minishell: cd: Too many arguments\n", node->fd_out);
 		return (1);
 	}
+	if (node->next && node->next->flag == MS_WORD)
+		err = ms_cd_with_params(node);
+	else
+		err = ms_cd_without_params(node);
+	return (err);
+}
+
+static int	ms_cd_without_params(t_word *node)
+{
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (chdir(ms_getenv_lst(node->env_lst, "HOME")) == -1)
+	{
+		ft_putstr_fd("minishell: cd: no such file or dir\n", node->fd_out);
+		free(pwd);
+		return (1);
+	}
+	if (ms_update_env(node->env_lst, "OLDPWD=", pwd) != 0)
+		return (1);
+	free(pwd);
+	pwd = getcwd(NULL, 0);
+	if (ms_update_env(node->env_lst, "PWD=", pwd) != 0)
+		return (1);
+	free(pwd);
+	return (0);
+}
+
+static int	ms_cd_with_params(t_word *node)
+{
+	char	*pwd;
+
 	pwd = getcwd(NULL, 0);
 	if (chdir(node->next->word) == -1)
 	{
@@ -42,7 +76,7 @@ int	ms_update_env(t_list *env_lst, char *ref, char *newvalue)
 {
 	while (env_lst)
 	{
-		if (ft_strncmp(ref, env_lst->content, ft_strlen(ref) == 0))
+		if (ft_strncmp(ref, env_lst->content, ft_strlen(ref)) == 0)
 		{
 			free(env_lst->content);
 			env_lst->content = ft_strjoin(ref, newvalue, 0);
@@ -53,38 +87,3 @@ int	ms_update_env(t_list *env_lst, char *ref, char *newvalue)
 	}
 	return (0);
 }
-
-/* IMPLEMENT
- * change the ms_export to be able to update the PWD var in ENV
- */
-
-/*
-int	ms_cd_go_back_one_dir(void)
-{
-	char	*prev_dir;
-	char	*curr_dir;
-	int		split_len;
-
-	curr_dir = getcwd(NULL, 0);
-	if (!curr_dir)
-	{
-		//settar a variável global
-		return (1);
-	}
-	split_len = ft_strlen(curr_dir) - ft_strlen(ft_strrchr(curr_dir, '/')) + 1;
-	if (split_len > 0)
-	{
-		prev_dir = ft_substr(curr_dir, 0, split_len);
-		if (!prev_dir)
-		{
-			free(curr_dir);
-			return (1);
-		}
-		if (chdir(prev_dir) != 0)
-			return (1);
-		free(prev_dir);
-	}
-	free(curr_dir);
-	return (0);
-}
-*/
