95ca2bcc5ed53eb233631480d375e38d10b87be1
no one cares <kevinfcdmelo@hotmail.com>
2023-04-25 16:36:20 -0300

    Fix:function with more than 4 arguments and correctec right path to libft

--- parser/ms_expand_env.c
+++ parser/ms_expand_env.c
@@ -19,37 +19,39 @@ char	*ms_expand_env(char *line, t_list *env)
 	ret_line = ft_calloc(ms_get_len_after_expansion(line, env) + 1,
 			sizeof(char));
 	if (ret_line)
-		ms_expand_env_util(line, 0, 0, ret_line, env);
+		ms_expand_env_util(line, ret_line, env);
 	free(line);
 	return (ret_line);
 }
 
-void	ms_expand_env_util(char *line, int i, int j, char *ret_line, t_list *env)
+void	ms_expand_env_util(char *line, char *ret_line, t_list *env)
 {
+	t_pos	aux;
 	char	must_expand;
 	char	in_double_quotes;
 
+	aux = (t_pos){0};	
 	must_expand = 1;
 	in_double_quotes = 1;
-	while (line[i])
+	while (line[aux.i])
 	{
-		if (line[i] == '\"' && must_expand)
+		if (line[aux.i] == '\"' && must_expand)
 			in_double_quotes ^= 1;
-		if (line[i] == '\'' && in_double_quotes)
+		if (line[aux.i] == '\'' && in_double_quotes)
 			must_expand ^= 1;
-		if (line[i] == '$' && must_expand && !ms_validate_env_name(&line[i]))
-			ms_expand_env_util2(line, ret_line, &i, &j, env);
+		if (line[aux.i] == '$' && must_expand && !ms_validate_env_name(&line[aux.i]))
+			ms_expand_env_util2(line, ret_line, &aux, env);
 		else
 		{
-			ret_line[j] = line[i];
-			i++;
-			j++;
+			ret_line[aux.j] = line[aux.i];
+			aux.i++;
+			aux.j++;
 		}
 	}
 	return ;
 }
 
-void	ms_expand_env_util2(char *line, char *ret_line, int *i, int *j, t_list *env)
+void	ms_expand_env_util2(char *line, char *ret_line, t_pos *aux, t_list *env)
 {
 	char			*env_value;
 	int				env_len;
@@ -62,16 +64,16 @@ void	ms_expand_env_util2(char *line, char *ret_line, int *i, int *j, t_list *env
 		else 
 			env_len = ms_count_decimal(g_exit_status);
 		env_value = ft_itoa(g_exit_status);
-		ft_memcpy(&ret_line[*j], env_value, env_len);
+		ft_memcpy(&ret_line[aux->j], env_value, env_len);
 	}
 	else
 	{
-		env_value = ft_substr(&line[*i], 1, ms_get_env_name_len(&line[*i]));
+		env_value = ft_substr(&line[aux->i], 1, ms_get_env_name_len(&line[aux->i]));
 		env_len = ft_strlen(ms_getenv_lst(env, env_value));
-		ft_memcpy(&ret_line[*j], ms_getenv_lst(env, env_value), env_len);
+		ft_memcpy(&ret_line[aux->j], ms_getenv_lst(env, env_value), env_len);
 	}
-	*i += ms_get_env_name_len(&line[*i]) + 1;
-	*j += env_len;
+	aux->i += ms_get_env_name_len(&line[aux->i]) + 1;
+	aux->j += env_len;
 	free(env_value);
 	return ;
 }
